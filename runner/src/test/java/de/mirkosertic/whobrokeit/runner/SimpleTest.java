3b08decaa2703439b0affe2075584e346055049d
Mirko Sertic <mirko.sertic@web.de>
2014-05-27 21:28:56 +0200

    Implemented a bug

--- runner/src/test/java/de/mirkosertic/whobrokeit/runner/SimpleTest.java
+++ runner/src/test/java/de/mirkosertic/whobrokeit/runner/SimpleTest.java
@@ -15,25 +15,6 @@ public class SimpleTest {
     public void testGreet() {
         UnderTestClass theClassUnderTest = new UnderTestClass();
         assertEquals("hello", theClassUnderTest.greet());
-    }
-
-    @Test
-    public void failingTest() {
-        throw new IllegalArgumentException();
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testExpectedException1() {
-        throw new IllegalArgumentException();
-    }
-
-    @Test(expected = RuntimeException.class)
-    public void testExpectedException2() {
-        throw new IllegalArgumentException();
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testFailingUnexpectedException() {
-        throw new RuntimeException();
+        throw new RuntimeException("This is strange!");
     }
 }
